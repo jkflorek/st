824e1faa7f3ddbcf9b2a01a4d6e47d0852edde52
Jeff Florek <jkflorek@gmail.com>
2020-08-25 17:28:19 -0400

    Fix mouse shortcuts for zoom and scroll

--- config.h
+++ config.h
@@ -258,12 +258,12 @@ ResourcePref resources[] = {
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
-	{ XK_NO_MOD,            Button4, kscrollup,      {.i =  1} },
-	{ XK_NO_MOD,            Button5, kscrolldown,    {.i =  1} },
-	{ TERMMOD,              Button4, zoom,           {.f = +1} },}
-	{ TERMMOD,              Button5, zoom,           {.f = -1} },
+	{ TERMMOD,              Button4,  zoom,           {.f = +1},        0 },
+	{ TERMMOD,              Button5,  zoom,           {.f = -1},        0 },
+	{ MODKEY,               Button4,  kscrollup,      {.i =  1},        0,       -1 },
+	{ MODKEY,               Button5,  kscrolldown,    {.i =  1},        0,       -1 },
+	{ XK_ANY_MOD,           Button4,  ttysend,        {.s = "\031"} },
+	{ XK_ANY_MOD,           Button5,  ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
